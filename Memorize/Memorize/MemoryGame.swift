e59828a26485297c53f3c3500079e58e928e2be6
MukhtarYusuf <mukhtar.yusuf@aun.edu.ng>
2023-06-05 20:15:36 -0400

    Update MemoryGame.swift
    Add properties

--- Memorize/Memorize/MemoryGame.swift
+++ Memorize/Memorize/MemoryGame.swift
@@ -8,4 +8,16 @@
 import Foundation
 
 struct MemoryGame<CardContent> where CardContent: Equatable {
+    private(set) var cards: [Card]
+    private var indexOfTheOneAndOnlyFaceUpCard: Int? {
+        get {
+            cards.indices.filter { cards[$0].isFaceUp }.oneAndOnly
+        }
+        set {
+            cards.indices.forEach {
+                cards[$0].isFaceUp = ($0 == newValue)
+            }
+        }
+    }
+    
 }
