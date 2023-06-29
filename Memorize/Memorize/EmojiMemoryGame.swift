746fe08d589af218414103b91c8c9ca55f232e9a
MukhtarYusuf <mukhtar.yusuf@aun.edu.ng>
2023-06-29 17:57:05 -0400

    Add Properties

--- Memorize/Memorize/EmojiMemoryGame.swift
+++ Memorize/Memorize/EmojiMemoryGame.swift
@@ -8,4 +8,18 @@
 import SwiftUI
 
 class EmojiMemoryGame: ObservableObject {
+    typealias Card = MemoryGame<String>.Card
+    
+    private static let emojis = ["🚂", "🚀", "🚁", "🚜", "🚗", "🚕", "🚙", "🛵", "🚝", "⛴", "⛵️", "🛰", "🛸", "🏍", "🚛", "🚜", "🚲"]
+    
+    @Published private var model = createMemoryGame()
+    
+    var cards: [Card] {
+        return model.cards
+    }
+    
 }
+
+/*
+ [2, 3, 1]
+ */
